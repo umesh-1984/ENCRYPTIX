eef549b2c7739d2da55b6abdb51ed0853bca79b4
umesh-1984 <umeshingole84@gmail.com>
2024-07-20 13:35:01 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> '''Design a simple calculator with basic arithmetic operations.
+... Prompt the user to input two numbers and a operation choice.
+... Perform the calculation and display the result...'''
+... 
+... '''Num1=float(input("Enter the First No.:"))
+... Num2=float(input("Enter the Second No.:"))
+... op=input("Enter Operator[+,-,*,/]:")
+... Result=0
+... if op=='+':
+...     Result=Num1+Num2
+... elif op=='-':
+...     Result=Num1-Num2
+... elif op=='*':
+...     Result=Num1*Num2
+... elif op=='/':
+...     Result=Num1/Num2
+... else:
+...     print("Invalid operator!!")
+... print(Num1,op,Num2,'=',Result)  '''
+...     
